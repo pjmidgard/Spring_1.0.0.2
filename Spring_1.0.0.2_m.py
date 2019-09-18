6cac5587bf99a8871cd0b50b6de3ba63f96f9799
Java Software <pj.midgard@yahoo.com>
2019-09-18 18:12:17 +0100

    Update Spring_1.0.0.2_m.py

--- Spring_1.0.0.2_m.py
+++ Spring_1.0.0.2_m.py
@@ -7,12 +7,12 @@ def f(x):
 
 if __name__ == '__main__':
     pool = Pool(processes=4)               # start 4 worker processes
-    result = pool.apply_async(f, [147455])     # evaluate "f(10)" asynchronously
+    result = pool.apply_async(f, [147456])     # evaluate "f(10)" asynchronously
     #print(result.get(timeout=1))           # prints "100" unless your computer is *very* slow
     #print(pool.map(f, range(147456)))          # prints "[0, 1, 4,..., 81]"
 
     
-    bnkw=pool.map(f, range(147455))
+    bnkw=pool.map(f, range(147456))
    
 
     import binascii
