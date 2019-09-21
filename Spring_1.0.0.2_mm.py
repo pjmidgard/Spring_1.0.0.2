86744f9c04eafa1245c9d840404cf5d50d88f4a1
Java Software <pj.midgard@yahoo.com>
2019-09-21 21:25:37 +0100

    Update Spring_1.0.0.2_mm.py

--- Spring_1.0.0.2_mm.py
+++ Spring_1.0.0.2_mm.py
@@ -168,7 +168,7 @@ if __name__ == '__main__':
                   
                
                     
-                szx=bin(aqwqs)[2:]
+                szx=qwaw
                 cvz=0
                 lenf=len(szx)
                 
